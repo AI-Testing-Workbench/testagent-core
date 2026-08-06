84a9e46bf2df22924844d2300305f4fd90279805
邱文斌 <qiuwenbin@piscifun.com>
2026-08-06 15:11:06 +0800

    替换logo

--- packages/ui/src/components/logo.tsx
+++ packages/ui/src/components/logo.tsx
@@ -1,17 +1,15 @@
 import { ComponentProps } from "solid-js"
+import logoLight from "../assets/images/kilo-light.png"
 
 export const Mark = (props: { class?: string }) => {
   return (
-    <svg
+    <img
       data-component="logo-mark"
       classList={{ [props.class ?? ""]: !!props.class }}
-      viewBox="0 0 16 20"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path data-slot="logo-logo-mark-shadow" d="M12 16H4V8H12V16Z" fill="var(--icon-weak-base)" />
-      <path data-slot="logo-logo-mark-o" d="M12 4H4V16H12V4ZM16 20H0V0H16V20Z" fill="var(--icon-strong-base)" />
-    </svg>
+      src={logoLight}
+      alt="logo"
+      style={{ "object-fit": "contain", display: "block", margin: "0 auto" }}
+    />
   )
 }
 
